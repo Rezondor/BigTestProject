9a1fb0fc6b4ab1e37b5b223d959e09c46edbdc21
Rezondor <eugenluzin@gmail.com>
2023-01-09 00:11:10 +0300

    Добавил шаблон команд, Реализовал главное окно

---
+++ BigTestProject/MainWindowViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace BigTestProject;
+
+class MainWindowViewModel: INotifyPropertyChanged
+{
+
+    #region Window Action
+
+    private CommandTemplate closeWindow;
+    private CommandTemplate maxWindow;
+    private CommandTemplate minWindow;
+
+    public CommandTemplate CloseWindow
+    {
+        get
+        {
+            return closeWindow ??= new CommandTemplate(obj =>
+            {
+                Window window = (Window)obj;
+                window.Close();
+            });
+        }
+    }
+    public CommandTemplate MaxWindow
+    {
+        get
+        {
+            return maxWindow ??= new CommandTemplate(obj =>
+            {
+                Window window = (Window)obj;
+                if (window.WindowState == WindowState.Maximized)
+                    window.WindowState = WindowState.Normal;
+                else
+                    window.WindowState = WindowState.Maximized;
+            });
+        }
+    }
+    public CommandTemplate MinWindow
+    {
+        get
+        {
+            return minWindow ??= new CommandTemplate(obj =>
+            {
+                Window window = (Window)obj;
+                window.WindowState = WindowState.Minimized;
+            });
+        }
+    }
+
+
+    #endregion
+
+
+
+    #region MVVM 
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName] string prop = "")
+    {
+        if (PropertyChanged != null)
+            PropertyChanged(this, new PropertyChangedEventArgs(prop));
+    }
+    #endregion
+}
