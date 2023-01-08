8e62b7c1136e9a41b8e96835204b0a4fc22e76b0
Rezondor <eugenluzin@gmail.com>
2023-01-08 21:49:24 +0300

    Добавил MainView, RandomGeneratorView, файл глобальных юзингов

---
+++ BigTestProject/Views/MainView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace BigTestProject.Views;
+
+/// <summary>
+/// Логика взаимодействия для MainView.xaml
+/// </summary>
+public partial class MainView : Page
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
